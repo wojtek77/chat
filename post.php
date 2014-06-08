3a69b5bffeb1082cda10aeca8e502dd8dd8d50cd
Wojciech Brüggemann <wojtek77@o2.pl>
2014-06-08 08:10:31 +0200

    Formatting code

--- post.php
+++ post.php
@@ -1,10 +1,12 @@
-<?
+<?php
+
 session_start();
-if(isset($_SESSION['name'])){
-	$text = $_POST['text'];
-	
-	$fp = fopen("log.html", 'a');
-	fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
-	fclose($fp);
+if (isset($_SESSION['name'])) {
+    $text = $_POST['text'];
+
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'>(" . date("g:i A") . ") <b>" . $_SESSION['name'] . "</b>: " . stripslashes(htmlspecialchars($text)) . "<br></div>");
+    fclose($fp);
 }
+
 ?>
