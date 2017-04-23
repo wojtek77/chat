f50b32d59b7b579c9d9c394e1b6bb229eb7ee00d
magikcypress <cyp@rouquin.me>
2017-04-24 00:46:07 +0200

    Change src index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ if (isset($_POST['enter'])) {
                 <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
             </form>
         </div>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script type="text/javascript" src=jquery.min.js"></script>
         <script type="text/javascript">
             // jQuery Document
             $(document).ready(function() {
